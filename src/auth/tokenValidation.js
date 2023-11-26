06618165ee7dbd4b9f289987f7dc4f652e291fb2
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2023-11-26 15:26:48 -0300

    feat: add token validation middleware

--- src/auth/tokenValidation.js
+++ src/auth/tokenValidation.js
@@ -1,2 +1,33 @@
 const jwt = require('jsonwebtoken');
 
+const userService = require('../services/userService');
+
+const secret = process.env.JWT_SECRET || 'secret';
+
+const extractToken = (bearerToken) => {
+  return bearerToken.split(' ')[1];
+};
+
+module.exports = async (req, res, next) => {
+  const bearerToken = req.header('Authorization');
+
+  if (!bearerToken) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+
+  const token = extractToken(bearerToken);
+
+  try {
+    const decoded = jwt.verify(token, secret);
+    const { status, data } = await userService.getUserById(decoded.user.id);
+
+    if (status !== 200) {
+      return res.status(401).json({ message: 'Expired or Invalid token' });
+    }
+
+    req.user = data.user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or Invalid token' });
+  }
+}
