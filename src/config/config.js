50a3ec2dba60e9e556bf1fcf3a89e37cc8dcfe86
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2023-12-30 21:28:23 -0300

    ci: update sequelize config

--- src/config/config.js
+++ src/config/config.js
@@ -1,15 +1,33 @@
 
-const config = {
+const production = {
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  dialect: 'mysql',
+};
+
+const test = {
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  dialect: 'mysql',
+};
+
+const development = {
   username: process.env.MYSQLUSER || 'root',
   password: process.env.MYSQLPASSWORD || 'password',
   database: process.env.MYSQLDATABASE || 'personal-blog-db',
   host: process.env.MYSQLHOST || 'database',
   port: process.env.MYSQLPORT || 3006,
   dialect: 'mysql',
-};
+}
 
 module.exports = {
-  development: config,
-  test: config,
-  production: config
+  development,
+  test,
+  production
 }
